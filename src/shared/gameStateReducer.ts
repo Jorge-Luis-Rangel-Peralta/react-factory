66d6edcc07c53bf4ed752656cb1da25a5b498319
Jorge Rangel <jorgerangel@Jorges-Mac-mini.local>
2021-01-30 08:04:37 -0600

    Only consume energy when some is left

--- src/shared/gameStateReducer.ts
+++ src/shared/gameStateReducer.ts
@@ -12,9 +12,23 @@ const gameStateReducer = (
 ): GameStateType => {
     switch (action.type) {
     case ActionTypeEnum.CLOCK_TICK:
+        if (state.energy === 0) {
+            return state
+        }
+
+        const energyConsumed = state.grid.reduce((total, row) => {
+            let newTotal = total
+            row.forEach((cell) => {
+                newTotal = newTotal + cell.energyConsumption
+            })
+            return newTotal
+        }, 0)
+
+        const energyAfterConsumption = state.energy - energyConsumed
+
         return {
             ...state,
-            energy: state.energy - 1,
+            energy: energyAfterConsumption,
         }
     default:
         throw new Error(`Invalid action type`)
